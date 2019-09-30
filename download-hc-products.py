7363955750f15747eb8cc8bfaf19c4efcbe03528
Slav Donchev <slav.donchev@gmail.com>
2019-09-30 10:06:58 +0300

    add version argument

--- download-hc-products.py
+++ download-hc-products.py
@@ -7,8 +7,18 @@ import os
 import sys
 
 # Define the TF realese to download
+if len(sys.argv) < 2:
+    print("no product name was provided.")
+    sys.exit(1)
+
 product = sys.argv[1]
-print('installing {0}'.format(product))
+
+if len(sys.argv) > 2:
+    ver = sys.argv[2]
+else:
+    ver = ""
+
+print('installing {0} {1}'.format(product, ver))
 install_path = '/usr/local/bin/'
 osys = 'linux'                                                                                                                                                                                                                   
 arch = 'amd64'
@@ -19,15 +29,18 @@ request = requests.get('https://releases.hashicorp.com/{0}/index.json'.format(pr
 # Find the latest stable relese
 tf_release_data = json.loads(request.content)
 tf_versions = tf_release_data["versions"].keys()
-tf_latest_ver = Version("0.0.0")
 
-for ver in tf_versions :
-    parsed_ver = parse(ver)
-    if not parsed_ver.is_prerelease :
-        if parsed_ver > tf_latest_ver :
-            tf_latest_ver = parsed_ver
 
-tf_latest_ver_builds = tf_release_data["versions"][tf_latest_ver.public]["builds"]
+if ver == "" :
+    tf_latest_ver = Version("0.0.0")
+    for ver in tf_versions :
+        parsed_ver = parse(ver)
+        if not parsed_ver.is_prerelease :
+            if parsed_ver > tf_latest_ver :
+                tf_latest_ver = parsed_ver
+    tf_latest_ver_builds = tf_release_data["versions"][tf_latest_ver.public]["builds"]
+else :
+    tf_latest_ver_builds = tf_release_data["versions"][ver]["builds"]
 
 # Get the URL for the required binnary type
 tf_latest_ver_url = ''
